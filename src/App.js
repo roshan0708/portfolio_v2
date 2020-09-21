f772cf556ded889d500c347f2e2372d9ffd94771
Roshan Kanwar <rkanwar0708@gmail.com>
2020-09-22 01:59:09 +0530

    structuring the project done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       Hello World
+      {/*
+      <Navbar/>
+      <Cover/>
+      <About/>
+      <WorkExperience/>
+      <Projects/>
+      <Contact/>
+      */}
     </div>
   );
 }
