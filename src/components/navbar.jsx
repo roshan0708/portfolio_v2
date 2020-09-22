11d3f34f1e8655744d19ef5a3cacc2f9b552102c
Roshan Kanwar <rkanwar0708@gmail.com>
2020-09-23 01:54:18 +0530

    navbar and cover design ready

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,7 @@ import { Nav, Navbar } from 'react-bootstrap';
 const NavbarComponent = () => {
     return (
       <>
-        <Navbar className="header-nav" sticky="top" collapseOnSelect bg="light" expand="lg">
+        <Navbar className="header-nav" fixed="top" collapseOnSelect bg="light" expand="lg">
           <Navbar.Brand href="#home" className="brand-name ml-5">
             Roshan.
           </Navbar.Brand>
@@ -12,14 +12,14 @@ const NavbarComponent = () => {
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ml-auto mr-5">
               <Nav.Link href="#home">Home</Nav.Link>
-              <p className="slash">/</p>
-              <Nav.Link href="#link">About</Nav.Link>
-              <p className="slash">/</p>
-              <Nav.Link href="#link">Experience</Nav.Link>
-              <p className="slash">/</p>
-              <Nav.Link href="#link">Projects</Nav.Link>
-              <p className="slash">/</p>
-              <Nav.Link href="#link">Contact</Nav.Link>
+              <p className="slash">|</p>
+              <Nav.Link href="#about">About</Nav.Link>
+              <p className="slash">|</p>
+              <Nav.Link href="#experience">Experience</Nav.Link>
+              <p className="slash">|</p>
+              <Nav.Link href="#projects">Projects</Nav.Link>
+              <p className="slash">|</p>
+              <Nav.Link href="#contact">Contact</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
