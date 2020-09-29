917446753478e19e5f91dc58b368143b14107855
Roshan Kanwar <rkanwar0708@gmail.com>
2020-09-30 02:35:53 +0530

    Updates

---
+++ precache-manifest.87c37f1080b7cc6411028b87799fdd28.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70f23b29848c76d5f6fcc5a81b7a6dd2",
+    "url": "/portfolio_v2/index.html"
+  },
+  {
+    "revision": "4e89ed42fa3ede15f80d",
+    "url": "/portfolio_v2/static/css/main.cb8124a0.chunk.css"
+  },
+  {
+    "revision": "f80abebfc5b0740a08e6",
+    "url": "/portfolio_v2/static/js/2.40ccdc36.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/portfolio_v2/static/js/2.40ccdc36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e89ed42fa3ede15f80d",
+    "url": "/portfolio_v2/static/js/main.eaa676da.chunk.js"
+  },
+  {
+    "revision": "092117d885c3545ea002",
+    "url": "/portfolio_v2/static/js/runtime-main.83767761.js"
+  },
+  {
+    "revision": "381af55d38e523ea981e73e577c6e608",
+    "url": "/portfolio_v2/static/media/bg-10.381af55d.png"
+  },
+  {
+    "revision": "d73e76cbbe3fb55faf0f6e186949792e",
+    "url": "/portfolio_v2/static/media/bg-2.d73e76cb.jpg"
+  },
+  {
+    "revision": "d4eb55f63542bd9d5339a579c295df7f",
+    "url": "/portfolio_v2/static/media/bg-6.d4eb55f6.jpg"
+  },
+  {
+    "revision": "1a9c668c1845508f82ccb484865d8851",
+    "url": "/portfolio_v2/static/media/project-1.1a9c668c.jpg"
+  },
+  {
+    "revision": "f77f39d8b71c3fdc59d45ac528745d6e",
+    "url": "/portfolio_v2/static/media/project-2.f77f39d8.jpg"
+  },
+  {
+    "revision": "bbd0adb87b51bb67ed93d3a0319e0259",
+    "url": "/portfolio_v2/static/media/project-3.bbd0adb8.jpg"
+  },
+  {
+    "revision": "56127ac1ab203f33d420f19938e1551a",
+    "url": "/portfolio_v2/static/media/project-4.56127ac1.jpg"
+  }
+]);
